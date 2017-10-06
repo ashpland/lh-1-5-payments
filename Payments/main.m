df17f14c1fae83fb5bc19778729e6dde3a0167ca
Andrew Shopland <hi@ashp.land>
2017-10-06 16:22:30 -0700

    Implement input

--- Payments/main.m
+++ Payments/main.m
@@ -6,12 +6,24 @@
 //  Copyright © 2017 Andrew. All rights reserved.
 //
 
+
 #import <Foundation/Foundation.h>
+#import "InputCollector.h"
+#define NSLog(FORMAT, ...) printf("%s\n", [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        int dollarValue = arc4random_uniform(1100) + 100;
+        NSLog(@"Thank you for shopping at Acme.com\nYour total today is $%d\nPlease select your payment method:\n1: Paypal, 2: Stripe, 3: Amazon", dollarValue);
+        
+        NSString *responseString = [InputCollector inputForPrompt:@""];
+        
+        int responseInt = (int)[responseString integerValue];
+        
+        NSLog(@"%d", responseInt);
+    
     }
     return 0;
 }
